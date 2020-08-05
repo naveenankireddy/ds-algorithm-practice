5619b0f65f8996958a53c369711cb617309b0617
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-08-05 23:27:33 +0530

    inserting Node

--- index.js
+++ index.js
@@ -1,23 +1,80 @@
-class Node{
-    constructor(data,next=null){
-        this.data = data;
-        thiis.next  = next;
-    }
+//datastructures
+
+//singly linked list creat, insert and delete
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    thiis.next = next;
+  }
 }
 
-class LinkedList{
-    constructor(){
-        this.head = null;
-    }
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 }
 
 let list = new LinkedList();
 
-LinkedList.prototype.insertStart = function(data){
-    let newNode = new Node(data)
+LinkedList.prototype.insertStart = function (data) {
+  let newNode = new Node(data);
 
-    newNode.next = this.head;
+  newNode.next = this.head;
 
+  this.head = newNode;
+  return this.head;
+};
+
+LinkedList.prototype.insertEnd = function (data) {
+  let newNode = new Node(data);
+
+  if (!this.head) {
     this.head = newNode;
-}
+    return this.head;
+  }
 
+  let tail = this.head;
+  while (tail.next != null) {
+    tail = tail.next;
+  }
+  tail.next = newNode;
+  return this.head;
+};
+
+// A helper function getAt() is used to define particular postion
+
+LinkedList.prototype.getAt = function (index) {
+  let counter = 0;
+  let node = this.head;
+  while (node) {
+    if (counter === index) {
+      return node;
+    }
+    counter++;
+    node = node.next;
+  }
+  return null;
+};
+
+//the insert at function is used to define at given index
+
+LinkedList.prototype.insertAt = function (data, index) {
+  //if the list is to be empty ie head = null
+  if (!this.head) {
+    this.head = new node(data);
+    return;
+  }
+  // if the new node is inserted at the front of the list that means before the head
+  if (index === 0) {
+    this.head = new Node(data, this.head);
+    return;
+  }
+
+  //we can use getAt() to find the previous node
+
+  const previous = this.getAt(index - 1);
+  let newNode = new Node(data);
+  newNode.next = previous.next;
+  previous.next = newNode;
+  return this.head;
+};
